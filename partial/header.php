cb69dd9363baed03c150879b22b14f6bdf015883
Jan Pabisiak <43246135+janpabisiak@users.noreply.github.com>
2021-07-17 18:20:46 +0200

    Update header.php

--- partial/header.php
+++ partial/header.php
@@ -1,4 +1,11 @@
 <?php
+/**
+* A simple script written in PHP that displays COVID-19 stats.
+* @license GNU General Public License
+* @author Jan Pabisiak
+* @copyright 2021 Jan Pabisiak
+*/
+
   require_once("functions.php");
 ?>
 <!DOCTYPE html>
