fa2bc7a68777544a4cb6c100c866f6a7854f1ba1
Jan Pabisiak <43246135+janpabisiak@users.noreply.github.com>
2021-07-17 18:23:50 +0200

    Update index.php

--- assets/css/index.php
+++ assets/css/index.php
@@ -1,3 +1,10 @@
 <?php
+/**
+* A simple script written in PHP that displays COVID-19 stats.
+* @license GNU General Public License
+* @author Jan Pabisiak
+* @copyright 2021 Jan Pabisiak
+*/
+
 header('Location: https://trackcovid.pl/404.php');
 ?>
